9f6edbb24e426e87a2946ad9aa328392e35e5eb4
Mykola Bova <bova.mykola@gmail.com>
2014-12-04 10:48:24 +0200

    1
    1

---
+++ code/Sedgewick/1.Base/1.Base/ex/1.1.3/Utils.java
@@ -0,0 +1,14 @@
+
+public class Utils {
+
+	public static boolean isInteger(String s) {
+		try { 
+			Integer.parseInt(s); 
+		} catch(NumberFormatException e) { 
+			return false; 
+		}
+		// only got here if we didn't return false
+		return true;
+	}
+	
+}
